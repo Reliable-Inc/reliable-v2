8be26f7c61a69a45df2887a64ee18dc555a0a59c
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-05-01 13:39:40 +0600

    Create `config.js` for bot I guess

---
+++ config.js
@@ -0,0 +1,10 @@
+const Configuration = {
+  developerIds: [
+    "1043709478031343647",
+    "783661052738011176",
+    "967657941937291265",
+  ],
+  guildId: "1029777893112418314", // Will be removed when it is public.
+};
+
+module.exports = { Configuration };
