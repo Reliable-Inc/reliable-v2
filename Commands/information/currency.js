0d6f4e955daeaab1aa4650d7db413c1824a43212
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-05-01 23:44:24 +0600

    Format the code

--- Commands/information/currency.js
+++ Commands/information/currency.js
@@ -7,9 +7,9 @@ const {
   ButtonBuilder,
   ActionRowBuilder,
   ButtonStyle,
-  Embed
+  Embed,
 } = require("discord.js");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 module.exports = {
   developer: false,
@@ -18,15 +18,24 @@ module.exports = {
     .setDescription("Converts one currency to another.")
     .setNSFW(false)
     .addStringOption((op) =>
-    op.setName("from").setDescription("The currency you want to convert from.").setRequired(true)
-  )
+      op
+        .setName("from")
+        .setDescription("The currency you want to convert from.")
+        .setRequired(true)
+    )
     .addStringOption((op) =>
-  op.setName("to").setDescription("The currency you want to convert to.").setRequired(true)
-)
+      op
+        .setName("to")
+        .setDescription("The currency you want to convert to.")
+        .setRequired(true)
+    )
     .addIntegerOption((op) =>
-op.setName("amount").setDescription("The amount of currency you want to convert.").setRequired(true)
-)
-.setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
+      op
+        .setName("amount")
+        .setDescription("The amount of currency you want to convert.")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
 
   /**
    *
@@ -35,44 +44,48 @@ op.setName("amount").setDescription("The amount of currency you want to convert.
    */
 
   async execute(interaction, client) {
-    const from = interaction.options.getString('from');
-    const touwu = interaction.options.getString('to');
-    const amount = interaction.options.getInteger('amount');
+    const from = interaction.options.getString("from");
+    const touwu = interaction.options.getString("to");
+    const amount = interaction.options.getInteger("amount");
 
-    const response = await fetch(`https://api.exchangeratesapi.io/latest?base=${from}`);
+    const response = await fetch(
+      `https://api.exchangeratesapi.io/latest?base=${from}`
+    );
     const data = await response.json();
     const rate = data.rates[touwu];
     const converted = (amount * rate).toFixed(2);
 
     const CurrencyExchange = new EmbedBuilder()
       .setTitle("Currency")
-      .addFields(
-        {
-          name: "__Exchange Information__",
-          value: `**\`•\` Converted**: **\`${converted}\`**`
-        }
-      )
+      .addFields({
+        name: "__Exchange Information__",
+        value: `**\`•\` Converted**: **\`${converted}\`**`,
+      })
       .setColor("#2F3136")
       .setFooter({ text: "©2022 - 2023 | Reliable" })
       .setTimestamp();
 
-      const components = new ActionRowBuilder().addComponents(
-          new ButtonBuilder()
-            .setCustomId("YES")
-            .setLabel(`From: ${from}`)
-            .setStyle("Secondary")
-            .setDisabled(true),
-          new ButtonBuilder()
-            .setCustomId("NO")
-            .setLabel(`To: ${touwu}`)
-            .setStyle("Secondary")
-            .setDisabled(true),
-            new ButtonBuilder()
-            .setCustomId("NOw")
-            .setLabel(`Amount: ${amount}`)
-            .setStyle("Secondary")
-            .setDisabled(true)
-      )
-    await interaction.editReply({ embeds: [CurrencyExchange], components: [components], ephemeral: true });
+    const components = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("YES")
+        .setLabel(`From: ${from}`)
+        .setStyle("Secondary")
+        .setDisabled(true),
+      new ButtonBuilder()
+        .setCustomId("NO")
+        .setLabel(`To: ${touwu}`)
+        .setStyle("Secondary")
+        .setDisabled(true),
+      new ButtonBuilder()
+        .setCustomId("NOw")
+        .setLabel(`Amount: ${amount}`)
+        .setStyle("Secondary")
+        .setDisabled(true)
+    );
+    await interaction.editReply({
+      embeds: [CurrencyExchange],
+      components: [components],
+      ephemeral: true,
+    });
   },
 };
