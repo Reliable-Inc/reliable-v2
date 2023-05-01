932b32273c8cad3f03fc3a617aae1dfe7a3f1b38
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-05-01 13:21:07 +0600

    Format the code

--- checker.js
+++ checker.js
@@ -20,29 +20,65 @@ const Token = process.env["Token"]?.toString();
 
 // ConfiguratonError
 if (!Token) {
-const errorMessage = chalk.cyan("[ ANTICRASH ]") +
-chalk.white.bold(" | ") +
-chalk.blue(`${new Date().toLocaleDateString()}`) +           chalk.white.bold(" | ") + chalk.whiteBright("The token isn't provided in .env folder.") + chalk.white.bold(" | ") + chalk.yellow.bold("Solution") + chalk.white(": ") + chalk.bold.greenBright(`Please provide the MongoDB URL in your environment variables.`)
-throw new ConfigurationError(`${errorMessage}`);
+  const errorMessage =
+    chalk.cyan("[ ANTICRASH ]") +
+    chalk.white.bold(" | ") +
+    chalk.blue(`${new Date().toLocaleDateString()}`) +
+    chalk.white.bold(" | ") +
+    chalk.whiteBright("The token isn't provided in .env folder.") +
+    chalk.white.bold(" | ") +
+    chalk.yellow.bold("Solution") +
+    chalk.white(": ") +
+    chalk.bold.greenBright(
+      `Please provide the MongoDB URL in your environment variables.`
+    );
+  throw new ConfigurationError(`${errorMessage}`);
 }
 if (!process.env["MongoDB"]) {
-const errorMessage = chalk.cyan("[ ANTICRASH ]") +
-chalk.white.bold(" | ") +
-chalk.blue(`${new Date().toLocaleDateString()}`) +           chalk.white.bold(" | ") + chalk.whiteBright("The MongoDB URL isn't provided in .env folder.") + chalk.white.bold(" | ") + chalk.yellow.bold("Solution") + chalk.white(": ") + chalk.bold.greenBright(`Please provide the MongoDB URL in your environment variables.`)
-throw new ConfigurationError(`${errorMessage}`);
+  const errorMessage =
+    chalk.cyan("[ ANTICRASH ]") +
+    chalk.white.bold(" | ") +
+    chalk.blue(`${new Date().toLocaleDateString()}`) +
+    chalk.white.bold(" | ") +
+    chalk.whiteBright("The MongoDB URL isn't provided in .env folder.") +
+    chalk.white.bold(" | ") +
+    chalk.yellow.bold("Solution") +
+    chalk.white(": ") +
+    chalk.bold.greenBright(
+      `Please provide the MongoDB URL in your environment variables.`
+    );
+  throw new ConfigurationError(`${errorMessage}`);
 }
 if (!process.env["MongoDB"].includes("mongodb+srv://")) {
-const errorMessage = chalk.cyan("[ ANTICRASH ]") +
-chalk.white.bold(" | ") +
-chalk.blue(`${new Date().toLocaleDateString()}`) +           chalk.white.bold(" | ") + chalk.whiteBright("You've provided an invalid MongoDB Url.") + chalk.white.bold(" | ") + chalk.yellow.bold("Solution") + chalk.white(": ") + chalk.bold.greenBright(`Please check the MongoDB URL in your environment variables.`)
-throw new ConfigurationError(`${errorMessage}`);
+  const errorMessage =
+    chalk.cyan("[ ANTICRASH ]") +
+    chalk.white.bold(" | ") +
+    chalk.blue(`${new Date().toLocaleDateString()}`) +
+    chalk.white.bold(" | ") +
+    chalk.whiteBright("You've provided an invalid MongoDB Url.") +
+    chalk.white.bold(" | ") +
+    chalk.yellow.bold("Solution") +
+    chalk.white(": ") +
+    chalk.bold.greenBright(
+      `Please check the MongoDB URL in your environment variables.`
+    );
+  throw new ConfigurationError(`${errorMessage}`);
 }
 
 if (!process.env["ClientId"]) {
-const errorMessage = chalk.greenBright("[ ANTICRASH ]") +
-chalk.white.bold(" | ") +
-chalk.blue(`${new Date().toLocaleDateString()}`) +           chalk.white.bold(" | ") + chalk.whiteBright("Client ID isn't provided in .env file") + chalk.white.bold(" | ") + chalk.yellow.bold("Solution") + chalk.white(": ") + chalk.bold.cyanBright(`Please make sure to add the Client ID to the .env file and restart the bot.`)
-throw new ConfigurationError(`${errorMessage}`);
+  const errorMessage =
+    chalk.greenBright("[ ANTICRASH ]") +
+    chalk.white.bold(" | ") +
+    chalk.blue(`${new Date().toLocaleDateString()}`) +
+    chalk.white.bold(" | ") +
+    chalk.whiteBright("Client ID isn't provided in .env file") +
+    chalk.white.bold(" | ") +
+    chalk.yellow.bold("Solution") +
+    chalk.white(": ") +
+    chalk.bold.cyanBright(
+      `Please make sure to add the Client ID to the .env file and restart the bot.`
+    );
+  throw new ConfigurationError(`${errorMessage}`);
 }
 // ConfigurationTypeError
 if (typeof Token !== "string") {
