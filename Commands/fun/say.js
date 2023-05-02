497c59f99bc8a45d2007cc7d79194645a9548f5e
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-05-02 19:18:32 +0600

    Update say.js

--- Commands/fun/say.js
+++ Commands/fun/say.js
@@ -3,11 +3,12 @@ const ms = require("ms");
 const list = require("badwords-list");
 const words = require("./bad-words.json");
 const {
-    ChatInputCommandInteraction,
-    SlashCommandBuilder,
-    Client,
-    EmbedBuilder
-  } = require("discord.js");
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  Client,
+  EmbedBuilder,
+} = require("discord.js");
+const cooldown = new CommandCooldown("earnCash", ms("50s"));
 
 module.exports = {
   developer: false,
@@ -15,7 +16,10 @@ module.exports = {
     .setName("say")
     .setDescription("Replies with what you've said.")
     .addStringOption((op) =>
-      op.setName("message").setDescription("The message to say.").setRequired(true)
+      op
+        .setName("message")
+        .setDescription("The message to say.")
+        .setRequired(true)
     ),
 
   /**
@@ -24,66 +28,81 @@ module.exports = {
    * @param {Client} client The client instance.
    */
   async execute(interaction, client) {
+    const userCooldowned = await cooldown.getUser(interaction.user.id);
+
+    if (userCooldowned) {
+      const timeLeft = msToMinutes(userCooldowned.msLeft, false);
+      return interaction.reply({
+        content: `You are in a cooldown. Time left: ${timeLeft.seconds}`,
+      });
+    }
     const toSay = interaction.options.getString("message");
-
+    const excludedWords = ["Hello"];
     // Check for bad words
-    const badWords = Object.values(words).concat(list.array);
-    if (badWords.some((word) => toSay.toLowerCase().includes(word))) {
-
-        const embed = new EmbedBuilder()
+    const badWords = Object.values(words.words).concat(list.array);
+    if (
+      badWords.some(
+        (word) =>
+          toSay.toLowerCase().includes(word) && !excludedWords.includes(word)
+      )
+    ) {
+      const embed = new EmbedBuilder()
         .setTitle("Say - Warning")
         .setColor("#2F3136")
         .setFooter({ text: "©2022 - 2023 | Reliable" })
-        .setDescription(`**<:reliable_wrong:1043155193077960764> | \`Please watch your language! Your message contains inappropriate words.\`**`)
-        .addFields(
-          {
-              name: "__The Bad Word__",
-              value: `||**\`${toSay}\`**||`
-          }
+        .setDescription(
+          `**<:reliable_wrong:1043155193077960764> | \`Please watch your language! Your message contains inappropriate words.\`**`
         )
-        
-        return interaction.reply({ embeds: [embed], ephemeral: true })
+        .addFields({
+          name: "__The Bad Word__",
+          value: `||**\`${toSay}\`**||`,
+        });
 
+      return interaction.reply({ embeds: [embed], ephemeral: true });
     }
 
     if (toSay.includes("@")) {
-        const embed = new EmbedBuilder()
+      const embed = new EmbedBuilder()
         .setTitle("Say - Warning")
         .setColor("#2F3136")
         .setFooter({ text: "©2022 - 2023 | Reliable" })
-        .setDescription(`**<:reliable_wrong:1043155193077960764> | \`Sorry, but you cannot send messages with mentions.\`**`)
-        
-        return interaction.reply({ embeds: [embed], ephemeral: true })
+        .setDescription(
+          `**<:reliable_wrong:1043155193077960764> | \`Sorry, but you cannot send messages with mentions.\`**`
+        );
+
+      return interaction.reply({ embeds: [embed], ephemeral: true });
     }
     try {
       await interaction.channel.send(toSay);
       const embed = new EmbedBuilder()
-      .setTitle("Say - Send")
-      .setColor("#2F3136")
-      .setFooter({ text: "©2022 - 2023 | Reliable" })
-      .setDescription(`*❝${toSay}🙷* - __**${interaction.user.tag}**__`)
-      .addFields(
-        {
-            name: "__Status__",
-            value: `<:reliable_right:1042843202429919272> **\`Successfully sent.\`**`
-        }
-      )
-      
-      return interaction.reply({ embeds: [embed], ephemeral: true })
+        .setTitle("Say - Send")
+        .setColor("#2F3136")
+        .setFooter({ text: "©2022 - 2023 | Reliable" })
+        .setDescription(`*❝${toSay}🙷* - __**${interaction.user.tag}**__`)
+        .addFields({
+          name: "__Status__",
+          value: `<:reliable_right:1042843202429919272> **\`Successfully sent.\`**`,
+        });
+
+      return interaction.reply({ embeds: [embed], ephemeral: true });
     } catch (error) {
       console.error(error);
       const embed = new EmbedBuilder()
-      .setTitle("Say - Error")
-      .setColor("#2F3136")
-      .setFooter({ text: "©2022 - 2023 | Reliable" })
-      .setDescription(`<:reliable_wrong:1043155193077960764> | **\`Sorry, an error occurred while trying to send your message.\`**`)
-      .addFields(
-        {
-            name: "__Status__",
-            value: `**\`Successfully sent.\`**`
-        }
-      )
-      return interaction.reply({ embeds: [embed], ephemeral: true })
+        .setTitle("Say - Error")
+        .setColor("#2F3136")
+        .setFooter({ text: "©2022 - 2023 | Reliable" })
+        .setDescription(
+          `<:reliable_wrong:1043155193077960764> | **\`Sorry, an error occurred while trying to send your message.\`**`
+        )
+        .addFields({
+          name: "__Status__",
+          value: `**\`Successfully sent.\`**`,
+        });
+
+      return (
+        interaction.reply({ embeds: [embed], ephemeral: true }) &&
+        (await cooldown.addUser(interaction.user.id))
+      );
     }
   },
 };
