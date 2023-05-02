4b83d6805e654b8312462f664410b224fb510748
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-05-02 20:02:51 +0600

    Change color converter default to discordjs-colors-bundle
    Why did I change the color converter default DiscordJS to DiscordJS-Colors-Bundle?
    
    - Fast converting: DiscordJS-Colors-Bundle is a light weight module and also optimized so it checks hex and converts it to number in no time
    - Better Handling: DiscordJS-Colors-Bundle converter has better error handling like if it is a valid hex or not

--- Commands/test/ping.js
+++ Commands/test/ping.js
@@ -7,8 +7,9 @@ const {
   ButtonBuilder,
   SlashCommandBuilder,
   ButtonStyle,
-  PermissionFlagsBits
+  PermissionFlagsBits,
 } = require("discord.js");
+const { CustomHex } = require("discordjs-colors-bundle");
 
 module.exports = {
   developer: true,
@@ -23,36 +24,41 @@ module.exports = {
    * @param {Client} client
    */
   async execute(interaction, client) {
-        const embed = new EmbedBuilder()
-        .setTitle("Commands - Developer")
-        .setDescription(`> Oops, it looks like there was an error while executing that command. Here are some possible reasons why this happened:
+    const embed = new EmbedBuilder()
+      .setTitle("Commands - Developer")
+      .setDescription(
+        `> Oops, it looks like there was an error while executing that command. Here are some possible reasons why this happened:
 
 **\`•\`** The bot encountered an unexpected issue while trying to process your command.
 **\`•\`** The command was used incorrectly or with invalid input.
 **\`•\`** The bot does not have the necessary permissions to perform that action.
         
-__If you continue to experience issues, please reach out to the bot's developers for assistance. Thank you!__`)
-        .setColor("#2F3136")
-        .setFooter({ text: "©2022 - 2023 | Reliable" })
+__If you continue to experience issues, please reach out to the bot's developers for assistance. Thank you!__`
+      )
+      .setColor(CustomHex("#2F3136"))
+      .setFooter({ text: "©2022 - 2023 | Reliable" });
 
-
-        const topgg = new ActionRowBuilder().addComponents(
-          new ButtonBuilder()
-            .setLabel("Vote Reliable")
-            .setEmoji("<:reliable_topgg:1034324522305855561>")
-            .setStyle("Link")
-            .setURL("https://top.gg/bot/1030870443005071512?s=05fa7c98112c0"),
-          new ButtonBuilder()
-            .setLabel("Support Server")
-            .setEmoji("<:reliable_support:1031443305399074836>")
-            .setStyle(ButtonStyle.Link)
-            .setURL("https://dsc.gg/reliable-support"),
-          new ButtonBuilder()
-            .setLabel("Invite Reliable")
-            .setEmoji("<:reliable_invite:1031443216664371231>")
-            .setStyle("Link")
-            .setURL("https://dsc.gg/reliable-bot")
-        );
-        return interaction.reply({ embeds: [embed], components: [topgg], ephemeral: true });
+    const topgg = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setLabel("Vote Reliable")
+        .setEmoji("<:reliable_topgg:1034324522305855561>")
+        .setStyle("Link")
+        .setURL("https://top.gg/bot/1030870443005071512?s=05fa7c98112c0"),
+      new ButtonBuilder()
+        .setLabel("Support Server")
+        .setEmoji("<:reliable_support:1031443305399074836>")
+        .setStyle(ButtonStyle.Link)
+        .setURL("https://dsc.gg/reliable-support"),
+      new ButtonBuilder()
+        .setLabel("Invite Reliable")
+        .setEmoji("<:reliable_invite:1031443216664371231>")
+        .setStyle("Link")
+        .setURL("https://dsc.gg/reliable-bot")
+    );
+    return interaction.reply({
+      embeds: [embed],
+      components: [topgg],
+      ephemeral: true,
+    });
   },
 };
