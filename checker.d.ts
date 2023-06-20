1742ae96b4ef5ce2dc2a8238305cdb53a52eb360
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-06-20 14:57:53 +0600

    Delete js checker

--- checker.d.ts
+++
@@ -1,8 +0,0 @@
-export {};
-/**
- * @INFO
- * Bot Coded by Sohom829#8350 & Alpha•#7395
- * You can't use this codes without permissions!
- * Powered By Reliable Inc.
- */
-//# sourceMappingURL=checker.d.ts.map
