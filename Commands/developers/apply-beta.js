b5075f268055d1dc977ce806350f242927f3ce44
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-07-09 14:04:46 +0600

    Update apply-beta.js

--- Commands/developers/apply-beta.js
+++ Commands/developers/apply-beta.js
@@ -1,31 +1,41 @@
-const { ModalBuilder, SlashCommandBuilder, ActionRowBuilder, Client, ChatInputCommandInteraction, TextInputBuilder, TextInputStyle ,} = require("discord.js");
-import * as cb from "discordjs-colors-bundle";
+const {
+  ModalBuilder,
+  SlashCommandBuilder,
+  ActionRowBuilder,
+  Client,
+  ChatInputCommandInteraction,
+  TextInputBuilder,
+  TextInputStyle,
+} = require('discord.js');
+import * as cb from 'discordjs-colors-bundle';
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName("apply-beta")
-  .setDescription(`Kindly submit your formal application for our exclusive beta program participation.`),
-  
-  /** 
-  * @param {ChatInputCommandInteraction} interaction
-  * @param {Client} client
-  */
-  
+    .setName('apply-beta')
+    .setDescription(
+      `Kindly submit your formal application for our exclusive beta program participation.`
+    ),
+
+  /**
+   * @param {ChatInputCommandInteraction} interaction
+   * @param {Client} client
+   */
+
   async execute(interaction, client) {
     const modal = new ModalBuilder()
-    .setCustomId("bm")
-    .setTitle("Beta Application")
-    
+      .setCustomId('bm')
+      .setTitle('Beta Application');
+
     const Username = new TextInputBuilder()
-    .setCustomId("userName")
-    .setLabel("May we know your Discord username, please?")
-    .setPlaceholder("exiled.reaper_.x") 
-    .setStyle(TextInputStyle.Short);
+      .setCustomId('userName')
+      .setLabel('May we know your Discord username, please?')
+      .setPlaceholder('exiled.reaper_.x')
+      .setStyle(TextInputStyle.Short);
 
     const WhyJustWHYBRUH = new TextInputBuilder()
-    .setCustomId("justWHY")
-    .setLabel("Why join our beta? Explain.") 
-    .setStyle(TextInputStyle.Paragraph);
+      .setCustomId('justWHY')
+      .setLabel('Why join our beta? Explain.')
+      .setStyle(TextInputStyle.Paragraph);
 
     const UserNameActionRow = new ActionRowBuilder().addComponents(Username);
     const WhyActionRow = new ActionRowBuilder().addComponents(WhyJustWHYBRUH);
@@ -33,8 +43,8 @@ module.exports = {
     modal.addComponents(UserNameActionRow, WhyActionRow);
 
     await interaction.showModal(modal);
-  }
-}
+  },
+};
 
 /**
  * @ Author Reliable Inc.
