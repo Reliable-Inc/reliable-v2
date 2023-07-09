cb2b25d4cb1a16110ddc4cc804ef045b65778f38
codeblitz97 <94981761+codeblitz97@users.noreply.github.com>
2023-07-09 18:05:12 +0000

    Update verification

--- Components/buttons/verification.js
+++ Components/buttons/verification.js
@@ -19,22 +19,30 @@ module.exports = {
       const member = await interaction.guild.members.fetch(interaction.user.id);
 
       if (!member.roles.cache.has(roleId)) {
-        member.roles.add(roleId);
-        interaction.reply({
-          content: 'You have been verified!',
-          ephemeral: true,
-        });
+         const embed = new EmbedBuilder()
+          .setColor('#2F3136')
+          .setTitle(`__Verified__`)
+          .setDescription(`We are pleased to inform you that your verification process has been successfully completed. Congratulations on achieving verification status, which grants you access to additional privileges and enhanced features.`)
+          .setFooter({ text: 'Reliable | Your trusted assistant' }) 
+
+          interaction.reply({ embeds: [embed], ephemeral: true })
       } else {
-        interaction.reply({
-          content: 'You are already verified!',
-          ephemeral: true,
-        });
+        const embed = new EmbedBuilder()
+        .setColor('#2F3136')
+        .setTitle(`__Already Verified__`)
+        .setDescription(`We would like to notify you that your verification status is already confirmed. As per our records, you have successfully completed the verification process, granting you access to the associated benefits and privileges.`)
+        .setFooter({ text: 'Reliable | Your trusted assistant' }) 
+
+        interaction.reply({ embeds: [embed], ephemeral: true })
       }
     } else {
-      interaction.reply({
-        content: 'Verification role not set. Contact an admin.',
-        ephemeral: true,
-      });
+      const embed = new EmbedBuilder()
+      .setColor('#2F3136')
+      .setTitle(`__Error__`)
+      .setDescription(`We regret to inform you that the verification role has not been assigned in our system. To resolve this matter, we kindly request you to reach out to an administrator for further assistance.`)
+      .setFooter({ text: 'Reliable | Your trusted assistant' }) 
+
+      interaction.reply({ embeds: [embed], ephemeral: true })
     }
   },
 };
