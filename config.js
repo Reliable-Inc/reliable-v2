1ea6f942a87aba23c00afb0c0f49944e243b108e
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-07-09 14:04:30 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
+import { PresenceUpdateStatus, ActivityType } from 'discord.js';
+
 const Configuration = {
   developerIds: [
-    "1043709478031343647",
-    "783661052738011176",
-    "967657941937291265",
+    '1043709478031343647',
+    '783661052738011176',
+    '967657941937291265',
   ],
-  guildId: "1029777893112418314", // Will be removed when it is public.
+  botPresence: {
+    status: PresenceUpdateStatus.DoNotDisturb,
+    activity: '/help',
+    activityType: ActivityType.Watching,
+  },
+  guildId: '1029777893112418314', // Will be removed when it is public.
 };
 
-module.exports = { Configuration };
+export default Configuration;
